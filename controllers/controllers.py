4eef517a58c4b67ad684b9b6450f6de6a020daac
Levis <levislibra@hotmail.com>
2020-10-14 14:46:18 -0300

    Update

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -7,19 +7,23 @@ class FinancieraMobbexWebhookController(http.Controller):
 
 	@http.route("/financiera.mobbex/webhook", type="json", auth="public", csrf=False, method=["POST"])
 	def webhook_listener(self, **kwargs):
+		print("webhook_listener")
 		webhook_type = None
-		if 'type' in kwargs:
-			webhook_type = kwargs['type']
+		print("kwargs: ", kwargs)
+		data = request.json
+		print("data: ", data)
+		if 'type' in data:
+			webhook_type = data['type']
 		if webhook_type == "subscription:registration":
-			_id = kwargs['data']['subscriber']['reference']
+			_id = data['data']['subscriber']['reference']
 			prestamo_id = request.env['financiera.prestamo'].sudo().browse(int(_id))
 			prestamo_id.mobbex_suscripcion_exitosa()
 		elif webhook_type == "subscription:change_source":
 			print("Webhook: Cambio de Método de Pago")
 		elif webhook_type == "subscription:execution":
-			_id = kwargs['data']['subscriber']['reference']
+			_id = data['data']['subscriber']['reference']
 			cuota_id = request.env['financiera.prestamo.cuota'].sudo().browse(int(_id))
-			cuota_id.mobbex_read_execution(kwargs['data'])
+			cuota_id.mobbex_read_execution(data['data'])
 		elif webhook_type == "subscription:subscriber:suspended":
 			print("Webhook: Suscriptor Suspendido")
 		elif webhook_type == "subscription:subscriber:active":
