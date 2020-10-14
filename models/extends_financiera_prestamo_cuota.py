e0325ee34d8ada36cd3453d57111182857603a15
Levis <levislibra@hotmail.com>
2020-10-14 11:12:25 -0300

    Update

--- models/extends_financiera_prestamo_cuota.py
+++ models/extends_financiera_prestamo_cuota.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from openerp import models, fields, api
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, date
 from dateutil import relativedelta
 import logging
 
@@ -24,27 +24,39 @@ class ExtendsFinancieraPrestamoCuota(models.Model):
 	def _mobbex_debit_execute(self):
 		cr = self.env.cr
 		uid = self.env.uid
-		fecha_actual = datetime.now()
-		primer_fecha = fecha_actual + relativedelta.relativedelta(days=self.mobbex_id.days_execute_on_expiration)
-		segunda_fecha = fecha_actual + relativedelta.relativedelta(days=self.mobbex_id.days_execute_on_expiration+1)
-		tercer_fecha = fecha_actual + relativedelta.relativedelta(days=self.mobbex_id.days_execute_on_expiration+2)
-		cuarta_fecha = fecha_actual + relativedelta.relativedelta(days=self.mobbex_id.days_execute_on_expiration+self.mobbex_id.days_execute_after)
-		fechas_ejecucion = [primer_fecha, segunda_fecha, tercer_fecha, cuarta_fecha]
-		cuotas_obj = self.pool.get('financiera.prestamo.cuota')
-		cuotas_ids = cuotas_obj.search(cr, uid, [
-			('prestamo_id.mobbex_debito_automatico', '=', True),
-			('prestamo_id.mobbex_suscripcion_suscriptor_confirm', '=', True),
-			('state', 'in', ('activa', 'judicial', 'incobrable')),
-			('saldo', '>', 0),
-			'|', ('fecha_vencimiento', 'in', fechas_ejecucion), 
-			('mobbex_program_execution_date', '=', fecha_actual),
-		])
+		fecha_actual = date.today()
+		company_obj = self.pool.get('res.company')
+		comapny_ids = company_obj.search(cr, uid, [])
 		_logger.info('Mobbex: iniciando debito de cuotas.')
 		count = 0
-		for _id in cuotas_ids:
-			cuota_id = cuotas_obj.browse(cr, uid, _id)
-			cuota_id.mobbex_subscriber_execution()
-			count += 1
+		for _id in comapny_ids:
+			company_id = company_obj.browse(cr, uid, _id)
+			if len(company_id.mobbex_id) > 0:
+				mobbex_id = company_id.mobbex_id
+				primer_fecha = fecha_actual + relativedelta.relativedelta(days=mobbex_id.days_execute_on_expiration)
+				segunda_fecha = fecha_actual + relativedelta.relativedelta(days=mobbex_id.days_execute_on_expiration+1)
+				tercer_fecha = fecha_actual + relativedelta.relativedelta(days=mobbex_id.days_execute_on_expiration+2)
+				cuarta_fecha = fecha_actual + relativedelta.relativedelta(days=mobbex_id.days_execute_after)
+				fechas_ejecucion = [
+					primer_fecha.__str__(),
+					segunda_fecha.__str__(),
+					tercer_fecha.__str__(),
+					cuarta_fecha.__str__(),
+				]
+				cuotas_obj = self.pool.get('financiera.prestamo.cuota')
+				cuotas_ids = cuotas_obj.search(cr, uid, [
+					('company_id', '=', company_id.id),
+					('prestamo_id.mobbex_debito_automatico', '=', True),
+					('prestamo_id.mobbex_suscripcion_suscriptor_confirm', '=', True),
+					('state', 'in', ('activa', 'judicial', 'incobrable')),
+					('saldo', '>', 0),
+					'|', ('fecha_vencimiento', 'in', fechas_ejecucion), 
+					('mobbex_program_execution_date', '=', fecha_actual),
+				])
+				for _id in cuotas_ids:
+					cuota_id = cuotas_obj.browse(cr, uid, _id)
+					cuota_id.mobbex_subscriber_execution()
+					count += 1
 		_logger.info('Mobbex: finalizo el debito de cuotas: %s cuotas ejecutadas', count)
 	
 	@api.one
