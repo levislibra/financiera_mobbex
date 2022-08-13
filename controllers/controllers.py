615b3266cb796915ec9fcdaaae0a156edcb66650
Levis Libra <levislibra@hotmail.com>
2022-08-13 20:36:18 -0300

    Update

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -8,7 +8,7 @@ import json
 _logger = logging.getLogger(__name__)
 class FinancieraMobbexWebhookController(http.Controller):
 
-	@http.route("/financiera.mobbex/webhook", auth="public", csrf=False)
+	@http.route("/financiera.mobbex/webhook", type='json', auth='none', cors='*', csrf=False)#, auth="public", csrf=False)
 	def webhook_listener(self, **post):
 		_logger.info('Mobbex: nuevo webhook.')
 		webhook_type = None
